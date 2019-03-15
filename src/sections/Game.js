58dca8c9184ae3bfd715332b7a48833f167d7cae
Karol Sitarz <ka.sitarz@gmail.com>
2019-03-15 15:27:13 +0100

    minor tweaks

---
+++ src/sections/Game.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Section from '../components/Section';
+
+export default class Game extends Component {
+  render () {
+    return (
+      <Section />
+    );
+  }
+}
