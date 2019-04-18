11271821e6a676be13247d8abbe6a3decfbb661d
Karol Sitarz <ka.sitarz@gmail.com>
2019-04-18 22:50:57 +0200

    remove fullscreen

--- src/sections/Login.js
+++ src/sections/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { changeSection } from '../store/actions';
-import screenfull from 'screenfull';
 import styled, { keyframes } from 'styled-components';
 
 import Section from '../components/Section';
@@ -24,24 +23,6 @@ const Title = styled.h1`
   color: transparent;
   font-size: 2em;
   animation: ${FadeIn} 1s ease backwards;
-`;
-
-const FSButton = styled.div`
-  position: absolute;
-  right: 1em;
-  bottom: 1em;
-  background: #f7f7f7;
-  padding: 0.5em 1.5em;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 1em;
-  fill: #b1b1b1;
-  box-shadow: 0 0.5em 1em 0 #00000011, 0 0 0 1px #00000005;
-
-  > svg {
-    width: .75em;
-  }
 `;
 
 class Login extends Component {
@@ -68,12 +49,6 @@ class Login extends Component {
           onClick={e => socket.comm('USER_LOGIN_PROMPT', { username: this.input })}
           primary>
           jump in!</Button>
-        <FSButton
-          onClick={e => screenfull.toggle()}>
-          <svg viewBox='0 0 800 800'>
-            <path d='M720,320V200A120,120,0,0,0,600,80H480V0H640A160,160,0,0,1,800,160V320H720ZM0,640V480H80V600A120,120,0,0,0,200,720H320v80H160A160,160,0,0,1,0,640ZM80,200V320H0V160A160,160,0,0,1,160,0H320V80H200A120,120,0,0,0,80,200ZM720,600V480h80V640A160,160,0,0,1,640,800H480V720H600A120,120,0,0,0,720,600Z' />
-          </svg>
-        </FSButton>
       </Section>
     );
   }
